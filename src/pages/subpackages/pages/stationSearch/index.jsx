63a94b85bcc095d76ac93d15eaeed45480f3d392
hm243695czl <1224311695@qq.com>
2022-01-10 17:52:42 +0800

    优化【职位搜索】

--- src/pages/subpackages/pages/stationSearch/index.jsx
+++ src/pages/subpackages/pages/stationSearch/index.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from '@tarojs/components'
+import { AtSearchBar, AtTag } from 'taro-ui';
 import './index.less'
 export default function User() {
+  const [val, setVal] = useState('');
+  const valChange = data => {
+    setVal(data);
+  };
+  const clickSearch = () => {
+    console.log(val);
+  };
   return (
     <View className='station-search-container'>
-      职位搜索
+      <View className='search-input'>
+        <AtSearchBar
+          showActionButton
+          placeholder='搜索职位、公司'
+          value={val}
+          onChange={valChange}
+          onActionClick={clickSearch}
+        />
+        <View className='search-history'>
+          <View className='title'>搜索历史</View>
+          <View className='tag-list'>
+            <AtTag>前端开发</AtTag>
+            <AtTag>web前端</AtTag>
+            <AtTag>汽车销售</AtTag>
+            <AtTag>中电金信</AtTag>
+            <AtTag>销售</AtTag>
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
