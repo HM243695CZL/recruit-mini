cd0c5df545ccf7fbf2cebc276c0d3ab1d1a55cc1
hm243695czl <1224311695@qq.com>
2022-01-10 23:09:37 +0800

    完成【职位搜索】界面

--- src/pages/subpackages/pages/stationSearch/index.jsx
+++ src/pages/subpackages/pages/stationSearch/index.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from '@tarojs/components'
 import { AtSearchBar, AtTag } from 'taro-ui';
+import StationList from '../../components/StationList';
 import './index.less'
 export default function User() {
   const [val, setVal] = useState('');
+  const [list, setList] = useState([1, 2, 3, 4, 5]);
+  const [historyList, setHistoryList] = useState(
+    ['前端开发', 'web前端', '汽车销售', '中电金信', '销售']);
   const valChange = data => {
     setVal(data);
   };
   const clickSearch = () => {
     console.log(val);
+  };
+  const clickHistory = data => {
+    setVal(data);
   };
   return (
     <View className='station-search-container'>
@@ -17,20 +24,32 @@ export default function User() {
           showActionButton
           placeholder='搜索职位、公司'
           value={val}
+          fixed={true}
           onChange={valChange}
           onActionClick={clickSearch}
         />
-        <View className='search-history'>
-          <View className='title'>搜索历史</View>
-          <View className='tag-list'>
-            <AtTag>前端开发</AtTag>
-            <AtTag>web前端</AtTag>
-            <AtTag>汽车销售</AtTag>
-            <AtTag>中电金信</AtTag>
-            <AtTag>销售</AtTag>
-          </View>
-        </View>
       </View>
+      {
+        val ? <View className='station-list-box'>
+          {
+            list.map((item, index) => <View className="box">
+              <StationList />
+              {
+                list.length - 1 === index ? '' : <View className='line' />
+              }
+            </View>)
+          }
+          </View>
+          :
+          <View className='search-history'>
+            <View className='title'>搜索历史</View>
+            <View className='tag-list'>
+              {
+                historyList.map(item => <AtTag onClick={e => clickHistory(item)}>{item}</AtTag>)
+              }
+            </View>
+          </View>
+      }
     </View>
   )
 }
