4a1c5e5c5eb86bce704d7c9fcce4b9866e3b23b9
hm243695czl <1224311695@qq.com>
2022-01-12 10:07:49 +0800

    优化完成【个人信息】页面

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Image, View} from '@tarojs/components'
+import {Image, View, Text} from '@tarojs/components'
 import {AtIcon} from 'taro-ui';
 import './index.less'
 
@@ -7,13 +7,17 @@ export default function User() {
   const [tabList] = useState([
     { text: '在线简历' },
     { text: '附件简历' },
-    { text: '我的面试' },
+    { text: '我的预约' },
+  ]);
+  const [iconList] = useState([
+    { icon: 'star', text: '我的收藏' },
+    { icon: 'star', text: '意见反馈' },
   ]);
   return (
     <View className='user-container'>
       <View className="head-card flex-between">
         <View className="left">
-          <View className="name">黄初舟</View>
+          <View className="name">张三</View>
           <View className="online-resume">
             <AtIcon className='edit-icon' value='edit' size='20' color='#fff' />
             我的在线简历
@@ -32,6 +36,27 @@ export default function User() {
             )
           })
         }
+      </View>
+      <View className='icon-list'>
+        {
+          iconList.map(item => {
+            return (
+              <View className='item-box flex-between'>
+                <View className='left flex-start'>
+                  <AtIcon value={item.icon} size='20' color='#787878' />
+                  <Text className='txt'>{item.text}</Text>
+                </View>
+                <AtIcon value='chevron-right' size='20' color='#787878' />
+              </View>
+            )
+          })
+        }
+      </View>
+      <View className='logout-btn'>
+        <View className='logout'>退出登录</View>
+      </View>
+      <View className='technical-support'>
+        HM243695CZL提供技术支持
       </View>
     </View>
   )
