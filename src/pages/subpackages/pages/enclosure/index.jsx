2c13a28a24ce7ab9a90487c3a0009bfa9b13b673
hm243695czl <1224311695@qq.com>
2022-01-12 17:46:18 +0800

    优化：【简历附件】页面

--- src/pages/subpackages/pages/enclosure/index.jsx
+++ src/pages/subpackages/pages/enclosure/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View, Text } from '@tarojs/components'
+import { AtIcon } from 'taro-ui';
 import './index.less'
 
 export default function Enclosure(){
   return (
-    <View className='Enclosure-container'>
-      附件简历
+    <View className='enclosure-container'>
+      <View className='tip flex-between'>
+        <Text>最多可以上传3份简历</Text>
+        <AtIcon value='close' size='20' color='#e2a34e' />
+      </View>
     </View>
   )
 }
