b015e0d6f18a007750dd61e9687f959acebe916c
hm243695czl <1224311695@qq.com>
2022-01-13 10:29:01 +0800

    优化：【附件简历】页面

---
+++ src/pages/subpackages/components/EnclosureList/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {View, Text} from '@tarojs/components';
+import { AtIcon, AtActionSheet, AtActionSheetItem } from 'taro-ui';
+import './index.less'
+export default function EnclosureList(props) {
+  const [isOpened, setIsOpened] = useState(false);
+  const showSheet = () => {
+    setIsOpened(true);
+  };
+  const hideSheet = () => {
+    setIsOpened(false);
+  };
+  return (
+    <>
+      <View className='enclosure-list flex-between'>
+        <View className='left flex-start'>
+          <AtIcon value='file-jpg' size='40' color='#787878' />
+          <View className='info'>
+            <Text className='enclosure-name'>我的简历.jpg</Text>
+            <Text className='detail'>
+              649.9KB 2021.04.08 08:50 上传
+            </Text>
+          </View>
+        </View>
+        <AtIcon onClick={showSheet} value='settings' size='20' color='#787878' />
+      </View>
+      <AtActionSheet
+        isOpened={isOpened}
+        cancelText='取消'
+        onClose={hideSheet}>
+        <AtActionSheetItem onClick={hideSheet}>
+          删除附件
+        </AtActionSheetItem>
+      </AtActionSheet>
+    </>
+  )
+}
--- src/pages/subpackages/pages/enclosure/index.jsx
+++ src/pages/subpackages/pages/enclosure/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from '@tarojs/components'
 import { AtIcon } from 'taro-ui';
+import EnclosureList from '../../components/EnclosureList';
 import './index.less'
 
 export default function Enclosure(){
+  const [list] = useState([1, 2, 3]);
   return (
     <View className='enclosure-container'>
       <View className='tip flex-between'>
         <Text>最多可以上传3份简历</Text>
         <AtIcon value='close' size='20' color='#e2a34e' />
+      </View>
+      <View className='enclosure-box'>
+        {
+          list.map(item => <EnclosureList />)
+        }
       </View>
     </View>
   )
