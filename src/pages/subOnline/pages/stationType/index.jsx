4b63cf0d8a9680ff91e134ce2a2832601fbe348c
hm243695czl <1224311695@qq.com>
2022-01-18 14:15:27 +0800

    优化：【职位分类】页面

--- src/pages/subOnline/pages/stationType/index.jsx
+++ src/pages/subOnline/pages/stationType/index.jsx
@@ -7,6 +7,7 @@ import './index.less'
 
 export default function StationType(){
   const [searchVal, setSearchVal] = useState('');
+  const [trueValue, setTrueValue] = useState('');
   const [currentType, setCurrentType] = useState('technology');
   const [infoList, setInfoList] = useState([]);
   const [typeList] = useState([
@@ -55,11 +56,22 @@ export default function StationType(){
       ]
     }
   ]);
+  const [searchList] = useState([
+    { text: '移动端测试(移动端测试)', value: 'mobile-test', type: '技术-测试' },
+    { text: 'Java(服务端开发工程师)', value: 'server-dev', type: '技术-后端开发' },
+    { text: 'Android(移动端开发工程师)', value: 'mobile-test', type: '技术-移动开发' },
+    { text: 'U3D(客户端开发工程师)', value: 'mobile-test', type: '技术-移动开发' },
+    { text: '产品经理(C端产品经理)', value: 'mobile-test', type: '产品-产品经理' },
+    { text: '移动产品经理(移动端产品经理)', value: 'mobile-test', type: '技术-产品经理' },
+  ]);
   const searchValChange = data => {
     setSearchVal(data);
+    if (data === '') {
+      setTrueValue('');
+    }
   };
   const clickSearch = () => {
-    console.log(searchVal);
+    setTrueValue(searchVal);
   };
   const changeType = currentTypeValue => {
     setCurrentType(currentTypeValue);
@@ -70,10 +82,17 @@ export default function StationType(){
     })
   };
   const clickInfoText = data => {
+    backPrevPage(data.text);
+  };
+  const clickSearchList = data => {
+    const value = data.text.split('(')[0];
+    backPrevPage(value);
+  };
+  const backPrevPage = value => {
     const pages = Taro.getCurrentPages();
     const prevPage = pages[pages.length - 2];
     prevPage.setData({
-      selectedStation: data.text
+      selectedStation: value
     });
     Taro.navigateBack({
       delta: 1
@@ -96,43 +115,56 @@ export default function StationType(){
         onChange={searchValChange}
         onActionClick={clickSearch}
       />
-      <View className='type-list-box flex-between'>
-        <View className='type-left'>
-          {
-            typeList.map(item => <View
-              className={
-                cx({
-                  'item-name': true,
-                  'active': item.value === currentType
-                })
-              }
-              onClick={e => changeType(item.value)}
-            >{item.text}</View>)
-          }
-        </View>
-        <View className='type-right'>
-          {
-            infoList.map(item => {
-              return (
-                <View className='right-box'>
-                  <View className='station-name'>{item.stationName}</View>
-                  <View className='station-list'>
-                    {
-                      item.list.map(ele => {
-                        return (
-                          <View className='station-list-item'>
-                            <View className='item-txt text-over' onClick={e => clickInfoText(ele)}>{ele.text}</View>
-                          </View>
-                        )
-                      })
-                    }
+      {
+        trueValue === '' ? <View className='type-list-box flex-between'>
+          <View className='type-left'>
+            {
+              typeList.map(item => <View
+                className={
+                  cx({
+                    'item-name': true,
+                    'active': item.value === currentType
+                  })
+                }
+                onClick={e => changeType(item.value)}
+              >{item.text}</View>)
+            }
+          </View>
+          <View className='type-right'>
+            {
+              infoList.map(item => {
+                return (
+                  <View className='right-box'>
+                    <View className='station-name'>{item.stationName}</View>
+                    <View className='station-list'>
+                      {
+                        item.list.map(ele => {
+                          return (
+                            <View className='station-list-item'>
+                              <View className='item-txt text-over' onClick={e => clickInfoText(ele)}>{ele.text}</View>
+                            </View>
+                          )
+                        })
+                      }
+                    </View>
                   </View>
+                )
+              })
+            }
+          </View>
+        </View> : <View className='type-item-box'>
+          {
+            searchList.map(item => {
+              return (
+                <View className='list-item' onClick={e => clickSearchList(item)}>
+                  <View className='name'>{item.text}</View>
+                  <View className='type'>{item.type}</View>
                 </View>
               )
             })
           }
         </View>
-      </View>
+      }
     </View>
   )
 }
